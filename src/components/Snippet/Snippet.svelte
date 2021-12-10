92843210d0f4e27ffbaf66cfd982242bca543f29
DDeenis <ddeenis46@gmail.com>
2021-12-10 16:51:21 +0200

    snippet component

---
+++ src/components/Snippet/Snippet.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import type { Snippet } from '../../query/snippets';
+
+	export let snippet: Snippet;
+</script>
+
+<div class="snippet-wrapper">
+	<div class="space-between">
+		<span class="snippet__info-text">{snippet.Language.name}</span>
+		<a class="snippet__info-text" href="/user/{snippet.User.id}"
+			>{`${snippet.User.firstName} ${snippet.User.lastName ?? ''}`}</a
+		>
+	</div>
+	<div class="snippet-title">
+		<a class="snippet-title__text" href="/snippets/{snippet.id}">{snippet.name}</a>
+	</div>
+</div>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+
+	.snippet-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		width: 100%;
+		max-width: 600px;
+		padding: 1rem 2rem;
+		margin: 0 auto;
+		background-color: #f6e05e;
+		border-radius: 0.75rem;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	.space-between {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+	}
+
+	.snippet__info-text {
+		font-size: 1rem;
+		background-color: #ecc94b;
+		padding: 5px 10px;
+		border-radius: 1.75rem;
+		color: #000;
+	}
+
+	.snippet-title {
+		background-color: #975a16;
+		width: 100%;
+		padding: 10px 0;
+		border-radius: 0.75rem;
+		text-align: center;
+	}
+
+	.snippet-title__text {
+		font-size: 1.125rem;
+		font-weight: 500;
+		color: #feebc8;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
 	import { query, setClient } from 'svelte-apollo';
 	import { client } from '../libs/apollo';
-	import Counter from '../components/Counter.svelte';
 	import { GetSnippets, GetSnippetsResponse } from '../query/snippets';
-	import 'chota';
+	import Snippet from '../components/Snippet/Snippet.svelte';
 
 	setClient(client);
 
 	const snippets = query<GetSnippetsResponse>(GetSnippets);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 {#if $snippets.loading}
 	Loading...
 {:else if $snippets.error}
 	Error: {$snippets.error}
 {:else}
-	<ul>
+	<div class="snippets-list">
 		{#each $snippets.data.querySnippet as snippet}
-			<li>{snippet.name}</li>
+			<Snippet {snippet} />
 		{/each}
-	</ul>
+	</div>
 {/if}
-<Counter initialValue={10} />
+
+<style>
+	.snippets-list {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+		justify-content: center;
+		align-items: center;
+		padding: 5rem 1rem;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: #c53030;
+	}
+</style>
