a069dccf849a815ed815b0c283c27afade9624a1
DDeenis <ddeenis46@gmail.com>
2021-12-10 21:59:40 +0200

    add prismjs

--- src/components/Snippet/Snippet.svelte
+++ src/components/Snippet/Snippet.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Accordion, AccordionItem } from 'svelte-accessible-accordion';
 	import type { Snippet } from '../../query/snippets';
 
 	export let snippet: Snippet;
+
+	const prismLangClassName = `language-${snippet.Language.name.toLowerCase()}`;
 </script>
 
 <div class="snippet-wrapper">
@@ -14,6 +17,13 @@
 	<div class="snippet-title">
 		<a class="snippet-title__text" href="/snippets/{snippet.id}">{snippet.name}</a>
 	</div>
+	<Accordion>
+		<AccordionItem title="Show code">
+			<pre style="margin: 0" class={prismLangClassName}>
+				<code>{snippet.code}</code>
+			</pre>
+		</AccordionItem>
+	</Accordion>
 </div>
 
 <style>
@@ -22,12 +32,13 @@
 	}
 
 	.snippet-wrapper {
+		box-sizing: border-box;
 		display: flex;
 		flex-direction: column;
 		gap: 10px;
 		width: 100%;
-		max-width: 600px;
-		padding: 1rem 2rem;
+		max-width: 500px;
+		padding: min(1rem, 3vw) min(2rem, 4vw);
 		margin: 0 auto;
 		background-color: #f6e05e;
 		border-radius: 0.75rem;
@@ -36,12 +47,15 @@
 
 	.space-between {
 		display: flex;
+		flex-wrap: wrap;
+		gap: 10px;
 		justify-content: space-between;
 		align-items: center;
 		width: 100%;
 	}
 
 	.snippet__info-text {
+		box-sizing: border-box;
 		font-size: 1rem;
 		background-color: #ecc94b;
 		padding: 5px 10px;
@@ -61,5 +75,29 @@
 		font-size: 1.125rem;
 		font-weight: 500;
 		color: #feebc8;
+	}
+
+	:global([data-accordion]) {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	:global([data-accordion-item] button) {
+		background: #ecc94b;
+		border: 0;
+		border-radius: 0.375rem 0.375rem 0 0;
+		box-sizing: border-box;
+		padding: 10px;
+		margin-top: 10px;
+		cursor: pointer;
+		width: 100%;
+		text-align: left;
+		font-size: 1rem;
+		font-weight: 500;
+	}
+
+	:global([data-accordion-item] button[disabled]) {
+		cursor: not-allowed;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,9 @@
 	import { query, setClient } from 'svelte-apollo';
 	import { client } from '../libs/apollo';
 	import { GetSnippets, GetSnippetsResponse } from '../query/snippets';
+	import Prism from 'prismjs';
 	import Snippet from '../components/Snippet/Snippet.svelte';
+	import 'prismjs/themes/prism-tomorrow.css';
 
 	setClient(client);
 
@@ -23,12 +25,18 @@
 
 <style>
 	.snippets-list {
-		display: flex;
-		flex-direction: column;
+		box-sizing: border-box;
+		display: grid;
+		grid-template-columns: repeat(2, 530px);
+		place-content: center;
 		gap: 1.5rem;
-		justify-content: center;
-		align-items: center;
 		padding: 5rem 1rem;
+	}
+
+	@media (max-width: 1200px) {
+		.snippets-list {
+			grid-template-columns: 1fr;
+		}
 	}
 
 	:global(body) {
