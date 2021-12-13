7d822ef0126c870b55685d9fc379a65acd62fa97
DDeenis <ddeenis46@gmail.com>
2021-12-13 17:55:54 +0200

    add monaco editor

---
+++ src/components/Monaco/MonacoEditor.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import type monaco from 'monaco-editor';
+	import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+	import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+	import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+	import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+	import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+	import { onMount } from 'svelte';
+
+	export let language: string;
+	export let value: string;
+
+	let divEl: HTMLDivElement = null;
+	let editor: monaco.editor.IStandaloneCodeEditor;
+	let Monaco;
+
+	onMount(async () => {
+		Monaco = await import('monaco-editor');
+
+		// @ts-ignore
+		self.MonacoEnvironment = {
+			getWorker(_, label) {
+				if (label === 'json') {
+					return new jsonWorker();
+				}
+				if (label === 'css' || label === 'scss' || label === 'less') {
+					return new cssWorker();
+				}
+				if (label === 'html' || label === 'handlebars' || label === 'razor') {
+					return new htmlWorker();
+				}
+				if (label === 'typescript' || label === 'javascript') {
+					return new tsWorker();
+				}
+				return new editorWorker();
+			},
+		};
+
+		editor = Monaco.editor.create(divEl, {
+			language: language || 'javascript',
+			theme: 'vs-dark',
+		});
+
+		return () => {
+			editor.dispose();
+		};
+	});
+
+	$: {
+		if (Monaco) {
+			Monaco.editor.setModelLanguage(editor.getModel(), language);
+		}
+	}
+</script>
+
+<div bind:this={divEl} class="code-editor" on:change={() => (value = editor.getValue())} />
+
+<style>
+	.code-editor {
+		min-height: 40vh;
+		width: 100%;
+	}
+</style>
---
+++ src/routes/snippets/create.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { writable } from 'svelte/store';
+
+	import MonacoEditor from '../../components/Monaco/MonacoEditor.svelte';
+
+	let name: string;
+	let language = writable<string>('javascript');
+	let code: string;
+</script>
+
+<form class="wrapper" on:submit|preventDefault={() => {}}>
+	<input type="text" placeholder="Name" bind:value={name} />
+	<input type="text" placeholder="Language" bind:value={$language} />
+	<MonacoEditor language={$language} value={code} />
+</form>
+
+<style>
+	.wrapper {
+		max-width: 600px;
+		margin: 0 auto;
+	}
+</style>
