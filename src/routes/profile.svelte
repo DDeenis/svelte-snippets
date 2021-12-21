69b3fdc8bf2a52d3432a372edf76e43e13702d7d
DDeenis <ddeenis46@gmail.com>
2021-12-21 19:07:17 +0200

    create profile page

---
+++ src/routes/profile.svelte
@@ -0,0 +1,29 @@
+<div class="wrapper">
+	<span class="title">Edit your site profile</span>
+</div>
+
+<style>
+	.wrapper {
+		min-height: 80vh;
+		max-width: 1000px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		margin-top: 2rem;
+		padding: min(1.75rem, 3vw) min(2.5rem, 4vw);
+		box-sizing: border-box;
+		border-radius: 0.75rem;
+		background-color: #f6e05e;
+		box-shadow: var(--box-shadow-md);
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	.title {
+		font-size: 1.5rem;
+		font-weight: 500;
+	}
+</style>
